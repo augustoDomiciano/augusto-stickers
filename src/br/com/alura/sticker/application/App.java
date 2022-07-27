4ce1c2a67ab845ae89a105be2c2092ce3cae0f70
Ana <augustoo.rd@gmail.com>
2022-07-27 17:34:40 -0300

    Alterando o texto dos filmes

--- src/br/com/alura/sticker/application/App.java
+++ src/br/com/alura/sticker/application/App.java
@@ -12,7 +12,7 @@ public class App {
         //Essa api tem a função de buscar imagens e transforma-las em figurinhas
         
 >>>>>>> master
-        // fazer uma conex�o HTTP e buscar os top 250 filmes
+        // fazer uma conex�o HTTP e buscar os top 250 filmes mais legais
 
         // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
          String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
