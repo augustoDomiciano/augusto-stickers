325d1f4294f74ea23d005a526e87033cb98acb94
Augusto Rogerio <augustoo.rd@gmail.com>
2022-07-26 21:22:19 -0300

    Mudando o numero de figurinhas geradas

--- src/br/com/alura/sticker/application/App.java
+++ src/br/com/alura/sticker/application/App.java
@@ -7,7 +7,7 @@ public class App {
 
 	public static void main(String[] args) throws Exception {
 
-        // fazer uma conexão HTTP e buscar os top 250 filmes
+        // fazer uma conexï¿½o HTTP e buscar os top 250 filmes
 
         // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
         // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
@@ -28,7 +28,7 @@ public class App {
 
         var geradora = new GeradoraDeFigurinhas();
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 3; i++) {
 
             Conteudo conteudo = conteudos.get(i);
 
